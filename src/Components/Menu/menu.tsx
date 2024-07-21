290a795478b484f067fa4d13689951e204d51d23
Maksymilian Olejnik <Maxx.olejnik@gmail.com>
2024-07-21 11:57:57 +0200

    ....

--- src/Components/Menu/menu.tsx
+++ src/Components/Menu/menu.tsx
@@ -27,6 +27,22 @@ const Menu = () => {
     });
 
 
+    const changeQuanity = (dishIndex: number, number: number) => {
+        const updatedDishData = dishData.map((dish, index) => {
+            if(index === dishIndex && ((dish.order.quantity > 0 && number < 0) || ((number > 0 && dishCount < dishLimit) && (cartContentSize + dish.order.quantity + number <= dishLimit) && number > 0))) {
+                dish.order.quantity += number;
+                setDishCount(dishCount + number);
+            }
+            else if(index === dishIndex && cartContentSize + dish.order.quantity + number > dishLimit) {        
+                data.setPopup((elements) =>
+                    [...elements as ReactElement[], <Popup message={`The order limit is set to ${dishLimit}${dishLimit - cartContentSize > 0 ? `, you can only add ${dishLimit - cartContentSize} more item/s` : ''}`} type="cart-update" />]
+                ) 
+            }
+    
+            return dish;
+        });
+        setDishData(updatedDishData);
+    };
 
     const addToCart = (dishIndex: number) => {
         const dish = dishData[dishIndex];
